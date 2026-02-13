de80815ad75845720e3ac036b797451f3e3cb0fe
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-02-13 17:39:19 -0300

    Add : novas aulas

--- academy/devdojo/maratonajava/javacore/Wnio/test/Aula147_ResolveTest01.java
+++ academy/devdojo/maratonajava/javacore/Wnio/test/Aula147_ResolveTest01.java
@@ -1,12 +1,10 @@
 package academy.devdojo.maratonajava.javacore.Wnio.test;
 
 import java.io.IOException;
-import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardCopyOption;
 
-public class Aula147_PathTest03 {
+public class Aula147_ResolveTest01 {
     public static void main(String[] args) throws IOException {
         Path dir = Paths.get("home/william");
         Path arquivo = Paths.get("dev/arquivo.txt");
