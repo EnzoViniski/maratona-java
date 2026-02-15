9e34a6691f78e14c0789356937c9812a6009d1a2
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-02-15 20:11:12 -0300

    Add : novas aulas

---
+++ academy/devdojo/maratonajava/javacore/Wnio/test/Aula156_PathMatcherTest01.java
@@ -0,0 +1,22 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.nio.file.*;
+
+public class Aula156_PathMatcherTest01 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("pasta/subpasta/file.txt");
+        Path path2 = Paths.get("pasta/subpasta/file.bkp");
+        Path path3 = Paths.get("pasta/subpasta/file.java");
+        matches(path1, "glob:**.bkp");
+        matches(path2, "glob:**.bkp");
+        matches(path3, "glob:**.{bkp,txt,java}");
+        matches(path3, "glob:**.????");
+        matches(path3, "glob:**/file.????");
+
+    }
+
+    private static void matches(Path path, String glob){
+        PathMatcher matcher = FileSystems.getDefault().getPathMatcher(glob);
+        System.out.println(glob+":"+matcher.matches(path));
+    }
+}
