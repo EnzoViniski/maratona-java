6d761c8c6460c60ac7563a2d1145cb4a589d02e1
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-02-15 11:33:03 -0300

    Add : novas aulas

---
+++ academy/devdojo/maratonajava/javacore/Wnio/test/Aula153_DirectoryStreamTest01.java
@@ -0,0 +1,21 @@
+package academy.devdojo.maratonajava.javacore.Wnio.test;
+
+import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class Aula153_DirectoryStreamTest01 {
+    public static void main(String[] args) {
+        Path dir = Paths.get(".");
+        try (DirectoryStream<Path> stream = Files.newDirectoryStream(dir)){
+            for (Path path : stream) {
+                System.out.println(path.getFileName());
+            }
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
