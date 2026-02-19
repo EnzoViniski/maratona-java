435e27456310e6074cdd441e44ff2c035b51945b
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-02-18 22:23:12 -0500

    Add : novas aulas

--- academy/devdojo/maratonajava/javacore/Ycolecoes/test/Aula161_EqualsTest01.java
+++ academy/devdojo/maratonajava/javacore/Ycolecoes/test/Aula161_EqualsTest01.java
@@ -5,8 +5,11 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.domain.Smartphone;
 public class Aula161_EqualsTest01{
     public static void main(String[] args) {
         Smartphone s1 = new Smartphone("1ABV2", "iPhone");
-        Smartphone s2 = s1;
+        Smartphone s2 = new Smartphone("1ABV2", "Samsung");
+        // a comparação está apenas no serialNumber, verificar a partir da regra de negócio
 
         System.out.println(s1.equals(s2));
+        System.out.println(s1.hashCode());
+        System.out.println(s2.hashCode());
     }
 }
---
+++ academy/devdojo/maratonajava/javacore/Ycolecoes/test/Aula166_ListTest01.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.Ycolecoes.test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula166_ListTest01 {
+    public static void main(String[] args) {
+        List<String> nome = new ArrayList<>();
+        nome.add("William");
+        nome.add("DevDojo Academy");
+        for (String nomes : nome){
+            System.out.println(nomes);
+        }
+        System.out.println("-----------");
+        nome.add("Suane");
+        for (int i = 0; i < nome.size(); i++) {
+            System.out.println(nome.get(i));
+        }
+    }
+}
