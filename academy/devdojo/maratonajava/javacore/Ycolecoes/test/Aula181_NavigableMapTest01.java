5082b524fb9631025984f14d73e7779dcfeaef58
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-02-25 22:41:15 +0000

    Add : novas aulas

--- academy/devdojo/maratonajava/javacore/Ycolecoes/test/Aula181_NavigableMapTest01.java
+++ academy/devdojo/maratonajava/javacore/Ycolecoes/test/Aula181_NavigableMapTest01.java
@@ -7,6 +7,8 @@ import java.util.TreeMap;
 public class Aula181_NavigableMapTest01 {
     public static void main(String[] args) {
         NavigableMap<String, String> map = new TreeMap<>();
+        // é um map que pode servir como por exemplo um dicionário, e nesse caso o TreeMap ordena a partir da key ou do comparator definido
+        // novamente, os elementos devem implementar comparable
         map.put("A", "Letra A");
         map.put("B", "Letra B");
         map.put("C", "Letra C");
