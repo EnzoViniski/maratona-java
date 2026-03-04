83c12e18f4733ae03ae0ba03b9889f9d4a9074f2
Enzo Viniski <viniskienzo@discente.ufg.br>
2026-03-04 14:45:34 +0000

    Add : novas aulas

---
+++ academy/devdojo/maratonajava/javacore/Zgenerics/test/Aula188_MetodoGenericoTest01.java
@@ -0,0 +1,17 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.test;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.domain.Barco;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Aula188_MetodoGenericoTest01 {
+    public static void main(String[] args) {
+        List<Barco> barcoList = criarArrayDeUmObjeto(new Barco("Canoa"));
+        System.out.println(barcoList);
+    }
+
+    private static <T> List<T> criarArrayDeUmObjeto(T t){
+        return List.of(t);
+    }
+}
