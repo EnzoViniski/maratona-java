c4363a16321d89f4a38d0be67114cdd7b8de6e19
Enzo Viniski <viniskienzo@gmail.com>
2026-05-08 11:23:28 -0300

    adicionando aulas

--- academy/devdojo/maratonajava/javacore/ZZClambdas/test/Aula199_MethodReferenceTest02.java
+++ academy/devdojo/maratonajava/javacore/ZZClambdas/test/Aula199_MethodReferenceTest02.java
@@ -1,5 +1,18 @@
 package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
 
-public class Aula199_MethodReferenceTest02 {
+import academy.devdojo.maratonajava.javacore.ZZClambdas.domain.Anime;
+import academy.devdojo.maratonajava.javacore.ZZClambdas.service.AnimeComparators;
 
+import java.util.ArrayList;
+import java.util.List;
+
+// Reference to an instance method of a particular object
+public class Aula199_MethodReferenceTest02 {
+    public static void main(String[] args) {
+        AnimeComparators animeComparators = new AnimeComparators();
+        List<Anime> animeList = new ArrayList<>(List.of(new Anime("Berserk", 43), new Anime("Naruto", 800), new Anime("One Piece", 120)));
+        animeList.sort(animeComparators::compareByEpisodesNonStatic);
+//        animeList.sort((a1, a2) -> animeComparators.compareByEpisodesNonStatic(a1, a2));
+        System.out.println(animeList);
+    }
 }
